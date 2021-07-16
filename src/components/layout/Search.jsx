99c6dae0effe42a5b923dfc0335685331a1ecc62
SantiagoAlvarze22 <sao2207@gmail.com>
2021-07-16 16:32:16 -0500

    recipes component

--- src/components/layout/Search.jsx
+++ src/components/layout/Search.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 export const Search = () => {
   return (
-    <div>
-      <h1>Search</h1>
+    <div className='searchRecipes'>
+      <form className='position-absolute start-50 translate-middle d-flex form-search'>
+        <input type='text' className='form-control me-3 inputSearch' />
+        <button className='form-control btn btn-primary btn-search'>
+          Search A Recipe
+        </button>
+      </form>
     </div>
   );
 };
